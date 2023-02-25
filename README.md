c157b008131ee15cc398b830218e4521c239d58e
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2023-02-24 22:50:21 -0300

    improviments

---
+++ README.md
@@ -0,0 +1,21 @@
+# Hangman Game
+
+>
+> Project developed with the intention of challenging myself and
+> learn more things with javascript.
+>
+
+Languages and Technologies:
+- VueJS;
+- HTML5;
+- CSS3;
+- JS
+
+## How to play?
+
+<div align="center">
+
+Click on here: [hangmangame](https://youngc0de.github.io/hangmangame/)
+
+<img src="./images/forGit.png" width="300">
+</div>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: '/hangmangame'
+    },
+    {
+      path: '/hangmangame',
+      name: 'Game',
+      component: () => import('@/views/Game.vue'),
+    },
+  ],
+});
+
+export default router;
